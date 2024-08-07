60a3522f967c8d018f8702379484b782be880e68
Emmanuel Guefif <eguefif@fastmail.com>
2024-08-07 11:48:15 -0400

    Correct mistake in username

--- kitty-install.sh
+++ kitty-install.sh
@@ -20,9 +20,8 @@ if [[ $? != 0 ]]; then
 	# Make xdg-terminal-exec (and hence desktop environments that support it use kitty)
 	echo 'kitty.desktop' > ~/.config/xdg-terminals.list
 	echo $PATH | grep /home/$USERNAME/.local/bin
-	USERNAME=$(who | awk '{print $1;exit}')
 	if [[ $? != 0 ]]; then
-		echo export PATH=$PATH:/home/$USERNAME/.local/bin >> ~/.bashrc
+		echo export PATH=$PATH:/home/$USER/.local/bin >> ~/.bashrc
 		export PATH=$PATH:/home/$USERNAME/.local/bin
 	fi
 fi

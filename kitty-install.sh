b906af8f574a4477d5d757b73a0008e324919116
Emmanuel Guefif <eguefif@fastmail.com>
2024-08-07 11:14:23 -0400

    add directory creation for kitty

--- kitty-install.sh
+++ kitty-install.sh
@@ -8,6 +8,8 @@ if [[ $? != 0 ]]; then
 	# your system-wide PATH)
 	ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
 	# Place the kitty.desktop file somewhere it can be found by the OS
+	mkdir -p ~/.local/share
+	mkdir -p ~/.local/share/applications
 	cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
 	# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
 	cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/

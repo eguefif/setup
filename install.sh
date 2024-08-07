458583d94dc153cef015008ac7088e3194cb3a80
Emmanuel Guefif <eguefif@fastmail.com>
2024-08-07 10:48:19 -0400

    handle kitty installation without sudo

--- install.sh
+++ install.sh
@@ -1,17 +1,32 @@
 #/bin/bash
 
-if [[ $1 == "linux" ]]; then
+USERNAME = who | awk '{print $1; exit}'
+
+uname | grep Linux
+if [[ $? == 0  ]]; then
+	whoami
+	if [[ $? != "root" ]]; then
+		echo "You need to execute this script as root"
+		exit
+	fi
 	PM="apt-get install -y "
-	apt-get update && apt-get upgrade
-elif [[ $1 == "mac" ]]; then
+	apt-get -y update && apt-get -y upgrade
+	which unzip
+	if [[ $? != 0 ]]; then
+		$PM unzip
+	fi
+	unzip Fira.zip -d ~/.fonts/
+else
 	which brew
 	if [[ $? != 0 ]]; then
 		/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	fi
 	PM="brew install -y"
-else
-	echo "You need to say if you want to install on mac or linux: bash installing.sh mac"
-	return
+	which unzip
+	if [[ $? != 0 ]]; then
+		$PM unzip
+	fi
+	unzip Fira.zip -d ~/Library/Fonts/
 fi
 
 echo "*********************************************************************"
@@ -22,25 +37,25 @@ echo "*********************************************************************"
 which kitty
 if [[ $? != 0 ]]; then
 	echo "Installing Kitty"
-	curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+	sudo -u $USERNAME curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
 	# Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in
 	# your system-wide PATH)
-	ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+	sudo -u $USERNAME ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
 	# Place the kitty.desktop file somewhere it can be found by the OS
-	cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+	sudo -u $USERNAME cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
 	# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
-	cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+	sudo -u $USERNAME cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
 	# Update the paths to the kitty and its icon in the kitty desktop file(s)
-	sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-	sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+	sudo -u $USERNAME sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+	sudo -u $USERNAME sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
 	# Make xdg-terminal-exec (and hence desktop environments that support it use kitty)
-	echo 'kitty.desktop' > ~/.config/xdg-terminals.list
+	sudo -u $USERNAME echo 'kitty.desktop' > ~/.config/xdg-terminals.list
 fi
 
 which tmux
 if [[ $? != 0 ]]; then
 	echo "Installing Tmux"
-	$PM libvent-dev ncurses-dev build-essential bison pkg-config
+	$PM libevent-dev ncurses-dev build-essential bison pkg-config
 	$PM tmux
 fi
 

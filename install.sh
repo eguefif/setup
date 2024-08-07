29d654f2d2a5353effe142b60fe86f394fe7a265
Emmanuel Guefif <eguefif@fastmail.com>
2024-08-07 09:01:44 -0400

    Add check for installation

--- install.sh
+++ install.sh
@@ -18,31 +18,43 @@ echo "****************Installing Emmanuel's terminal setup*****************"
 echo "*********************************************************************"
 
 
-echo "Installing Kitty"
-curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
-# Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in
-# your system-wide PATH)
-ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
-# Place the kitty.desktop file somewhere it can be found by the OS
-cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
-# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
-cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
-# Update the paths to the kitty and its icon in the kitty desktop file(s)
-sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
-sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
-# Make xdg-terminal-exec (and hence desktop environments that support it use kitty)
-echo 'kitty.desktop' > ~/.config/xdg-terminals.list
+which kitty
+if [[ $? != 0 ]]; then
+	echo "\nInstalling Kitty"
+	curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+	# Create symbolic links to add kitty and kitten to PATH (assuming ~/.local/bin is in
+	# your system-wide PATH)
+	ln -sf ~/.local/kitty.app/bin/kitty ~/.local/kitty.app/bin/kitten ~/.local/bin/
+	# Place the kitty.desktop file somewhere it can be found by the OS
+	cp ~/.local/kitty.app/share/applications/kitty.desktop ~/.local/share/applications/
+	# If you want to open text files and images in kitty via your file manager also add the kitty-open.desktop file
+	cp ~/.local/kitty.app/share/applications/kitty-open.desktop ~/.local/share/applications/
+	# Update the paths to the kitty and its icon in the kitty desktop file(s)
+	sed -i "s|Icon=kitty|Icon=$(readlink -f ~)/.local/kitty.app/share/icons/hicolor/256x256/apps/kitty.png|g" ~/.local/share/applications/kitty*.desktop
+	sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
+	# Make xdg-terminal-exec (and hence desktop environments that support it use kitty)
+	echo 'kitty.desktop' > ~/.config/xdg-terminals.list
+fi
 
-echo "Installing Tmux"
-$PM tmux
-$PM libvent
-$PM ncurses
+which tmux
+if [[ $? != 0 ]]; then
+	echo "Installing Tmux"
+	$PM tmux
+	$PM libvent
+	$PM ncurses
+fi
 
-echo "Installing Neovim"
-$PM neovim
+which nvim
+if [[ $? != 0 ]]; then
+	echo "Installing Neovim"
+	$PM neovim
+fi
 
-echo "Installing stow"
-$PM stow
+which stow
+if [[ $? != 0 ]]; then
+	echo "Installing stow"
+	$PM stow
+fi
 
 echo "Setting up git"
 git config --global core.editor "nvim"

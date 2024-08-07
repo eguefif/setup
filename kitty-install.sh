986c931f8844d7eda8c408df9d19b6ad702bc453
Emmanuel Guefif <eguefif@fastmail.com>
2024-08-07 11:40:10 -0400

    add kitty to path

--- kitty-install.sh
+++ kitty-install.sh
@@ -19,4 +19,9 @@ if [[ $? != 0 ]]; then
 	sed -i "s|Exec=kitty|Exec=$(readlink -f ~)/.local/kitty.app/bin/kitty|g" ~/.local/share/applications/kitty*.desktop
 	# Make xdg-terminal-exec (and hence desktop environments that support it use kitty)
 	echo 'kitty.desktop' > ~/.config/xdg-terminals.list
+	echo $PATH | grep /home/$USERNAME/.local/bin
+	if [[ $? != 0 ]]; then
+		echo export PATH=$PATH:/home/$USERNAME/.local/bin
+		export PATH=$PATH:/home/$USERNAME/.local/bin
+	fi
 fi

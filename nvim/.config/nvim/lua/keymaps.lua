45e5cc1c3b0adf35a1f6f196b40a1081dbef1aca
Emmanuel Guefif <eguefif@fastmail.com>
2025-01-04 10:40:10 -0500

    Add moves keymap

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -25,3 +25,11 @@ vim.keymap.set("n", "[e", diagnostic_goto(true, "ERROR"), { desc = "Jump to the 
 vim.keymap.set("n", "]e", diagnostic_goto(false, "ERROR"), { desc = "Jump to the previous Error" })
 vim.keymap.set("n", "[w", diagnostic_goto(true, "WARN"), { desc = "Jump to the next Warning" })
 vim.keymap.set("n", "]w", diagnostic_goto(false, "WARN"), { desc = "Jump to the previous Warning" })
+
+-- Move Lines
+vim.keymap.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+vim.keymap.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+vim.keymap.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })

d40316115cbf752d9525984b85198b607e87e152
Emmanuel Guefif <eguefif@fastmail.com>
2025-02-02 08:11:08 -0500

    Add trouble to nvim

--- nvim/.config/nvim/lua/plugins/lsp_config.lua
+++ nvim/.config/nvim/lua/plugins/lsp_config.lua
@@ -12,6 +12,20 @@ return {
         },
     },
     config = function()
+        local lspconfig = require('lspconfig')
+
+        lspconfig.volar.setup {
+            -- add filetypes for typescript, javascript and vue
+            filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+            init_options = {
+                vue = {
+                    -- disable hybrid mode
+                    hybridMode = false,
+                },
+            },
+        }
+        -- you must remove ts_ls setup
+        -- lspconfig.ts_ls.setup {}
         require("lspconfig").lua_ls.setup({})
         require("lspconfig").rust_analyzer.setup({
             settings = {

34433aea4a6c493cb3e1f704446d0c1322cd0758
Emmanuel Guefif <emmanuel.guefif@demarque.com>
2025-02-14 08:17:49 -0500

    Add some pluging and update zshrc

--- nvim/.config/nvim/lua/plugins/lsp_config.lua
+++ nvim/.config/nvim/lua/plugins/lsp_config.lua
@@ -12,6 +12,10 @@ return {
         },
     },
     config = function()
+        require("lspconfig").elixirls.setup({
+            -- Unix
+            cmd = { "/Users/eguefif/.local/share/nvim/mason/bin/elixir-ls" },
+        })
         require("lspconfig").lua_ls.setup({})
         require("lspconfig").rust_analyzer.setup({
             settings = {
@@ -29,7 +33,9 @@ return {
             filetypes = { "ruby" },
         })
         require("lspconfig").ruff.setup({})
+        require("lspconfig").clangd.setup({})
 
+        require("lspconfig").eslint.setup({})
         vim.api.nvim_create_autocmd("LspAttach", {
             callback = function(args)
                 local client = vim.lsp.get_client_by_id(args.data.client_id)
--- stow-config.sh
+++ stow-config.sh
@@ -3,3 +3,4 @@ stow -t $HOME -S nvim
 stow -t $HOME -S ghostty
 stow -t $HOME -S zsh
 stow -t $HOME -S ripgreprc
+stow -t $HOME -S lazygit

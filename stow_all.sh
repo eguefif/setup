ee4ef43b3cf3221cab7734b325c78cdec045c138
Emmanuel Guefif <eguefif@fastmail.com>
2025-12-26 14:26:37 +0100

    Add and configure tmux

--- stow_all.sh
+++ stow_all.sh
@@ -2,6 +2,7 @@
 
 stow --dotfiles -t $HOME nvim
 stow --dotfiles -t $HOME zshrc
+stow --dotfiles -t $HOME tmux
 stow --dotfiles -t $HOME ghostty
 stow --dotfiles -t $HOME gnome
 dconf load / < ~/.config/dconf/dconf-settings.ini

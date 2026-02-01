65303ee63765cea855d2a48d8a7c74bceaf1d9ff
Emmanuel Guefif <eguefif@fastmail.com>
2026-02-01 12:35:50 +0100

    Feb 1 Add bufdelete to avoid closing split when deleting a buffer.
    Add aliases in zhsrc:
    * cdvim to go directly in vim config
    * cdsetup to go directly in setup

--- nvim/dot-config/nvim/lua/plugins/bufferline.lua
+++ nvim/dot-config/nvim/lua/plugins/bufferline.lua
@@ -1,16 +1,22 @@
 return {
-    "akinsho/bufferline.nvim",
-    version = "*",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    lazy = false,
-    keys = {
-        {"[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer"},
-        {"]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer"},
-        {"<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer"},
-        {"<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer"},
-        {"<leader>bp", "<cmd>BufferLinePick<cr>", desc = "Pick buffer"},
-        {"<leader>bc", "<cmd>BufferLinePickClose<cr>", desc = "Close buffer"},
-        {"<leader>bd", "<cmd>bdelete<cr>", desc = "Delete current buffer"}
-    },
-    opts = {options = {close_command = "bdelete! %d"}}
+    {
+        "akinsho/bufferline.nvim",
+        version = "*",
+        dependencies = {"nvim-tree/nvim-web-devicons", "famiu/bufdelete.nvim"},
+        lazy = false,
+        keys = {
+            {"[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer"},
+            {"]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer"},
+            {"<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Previous buffer"},
+            {"<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer"},
+            {"<leader>bp", "<cmd>BufferLinePick<cr>", desc = "Pick buffer"},
+            {
+                "<leader>bc",
+                "<cmd>BufferLinePickClose<cr>",
+                desc = "Close buffer"
+            },
+            {"<leader>bd", "<cmd>Bdelete<cr>", desc = "Delete current buffer"}
+        },
+        opts = {options = {close_command = "Bdelete! %d"}}
+    }
 }

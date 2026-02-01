b6d5ab3de5b2b3881a0ce44cdf547dc9fb9c9e11
Emmanuel Guefif <eguefif@fastmail.com>
2026-02-01 09:16:50 +0100

    Add relative path for Telescope grep

--- nvim/dot-config/nvim/lua/config/telescope/grep.lua
+++ nvim/dot-config/nvim/lua/config/telescope/grep.lua
@@ -4,7 +4,27 @@ local make_entry = require("telescope.make_entry")
 local conf = require("telescope.config").values
 local M = {}
 
+-- Example, if the path is /home/eguefif/lab/my_project/src/db/parser/
+-- it will return src/db/parser
+local get_relative_path = function(path)
+    local project_path_splits = vim.split(vim.fn.getcwd(), "/")
+    local path_splits = vim.split(path, "/")
+    local new_path = ""
+    if #path_splits <= #project_path_splits then return "" end
+    for n = #project_path_splits, #path_splits, 1 do
+        new_path = new_path .. "/" .. path_splits[n]
+    end
+    return new_path
+end
+
 local live_grep_type = function(opts)
+    local prompt_title = "Multi grep"
+    if opts and opts.cwd then
+        local relative_path = get_relative_path(opts.cwd)
+        if #relative_path > 0 then
+            prompt_title = "Multi Grep (" .. relative_path .. ")"
+        end
+    end
     opts = opts or {}
     opts.cwd = opts.cwd or vim.fn.getcwd()
     local finder = finders.new_async_job({
@@ -34,9 +54,9 @@ local live_grep_type = function(opts)
 
     pickers.new(opts, {
         debounce = 100,
-        prompt_title = "Multi Grep",
+        prompt_title = prompt_title,
         finder = finder,
-        previewer = conf.grep_previewer(opts),
+        previewer = conf.grep_previewer({}),
         sorter = require("telescope.sorters").empty()
     }):find()
 end

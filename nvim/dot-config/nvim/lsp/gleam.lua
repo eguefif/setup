5d0ef81b63c4584a07108c9b4fdffc96922efa34
Emmanuel Guefif <eguefif@fastmail.com>
2026-02-01 23:00:30 +0100

    Update zsh for alias

--- nvim/dot-config/nvim/lsp/gleam.lua
+++ nvim/dot-config/nvim/lsp/gleam.lua
@@ -1,5 +1,6 @@
 return {
-    cmd = {"/home/eguefif/lab/gleam/bin/lsp.sh", "lsp"},
+    -- cmd = {"/home/eguefif/lab/gleam/bin/lsp.sh", "lsp"},
+    cmd = {"gleam", "lsp"},
     filetypes = {"gleam"},
     root_markers = {'.git', 'gleam.toml'}
 }

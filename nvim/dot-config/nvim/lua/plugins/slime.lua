5451ebe10be885fcb0e348e170241330ed363066
Emmanuel Guefif <emmanuel.guefif@demarque.com>
2026-06-09 12:18:14 +0200

    Update slime configuration

--- nvim/dot-config/nvim/lua/plugins/slime.lua
+++ nvim/dot-config/nvim/lua/plugins/slime.lua
@@ -3,17 +3,20 @@ return {
     {
         "jpalardy/vim-slime",
         keys = {
-            {"<leader>rc", "<cmd>SlimeConfig<cr>", desc = "Slime Config"},
-            {
-                "<leader>rr",
-                "<Plug>SlimeSendCell<BAR>/^# %%<CR>",
-                desc = "Slime Send Cell"
-            }
+            { "<leader>rc", "<cmd>SlimeConfig<cr>", desc = "Slime Config" },
+            { "<leader>rr", desc = "Slime Send Cell" },
+            { "<leader>rl", desc = "Slime Send Line" },
+            { "<leader>rr", desc = "Slime Send Selection", mode = "v" },
         },
-        config = function()
+        init = function()
             vim.g.slime_target = "tmux"
             vim.g.slime_cell_delimiter = "# %%"
             vim.g.slime_bracketed_paste = 1
+        end,
+        config = function()
+            vim.keymap.set("n", "<leader>rr", "<Plug>SlimeSendCell", { desc = "Slime Send Cell" })
+            vim.keymap.set("n", "<leader>rl", "<Plug>SlimeLineSend", { desc = "Slime Send Line" })
+            vim.keymap.set("v", "<leader>rr", "<Plug>SlimeRegionSend", { desc = "Slime Send Selection" })
         end
     }
 }
